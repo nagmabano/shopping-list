8428fca05758c5063ad25ca89cb0af83f3f6d63f
Nagma Bano <nagma.bano@sap.com>
2024-02-05 17:39:21 +0530

    shopping-list app with a button

--- app/src/main/java/com/learning/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/learning/myshoppinglistapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.learning.myshoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.learning.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
@@ -22,25 +27,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column (modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        ) {
+                        Button(onClick = { /*TODO*/ }, modifier = Modifier.align(CenterHorizontally)) {
+                            Text(text = "Add Item")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyShoppingListAppTheme {
-        Greeting("Android")
     }
 }
