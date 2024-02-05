b2eebd3d4325ad96590b912fab428825076e53ee
Nagma Bano <nagma.bano@sap.com>
2024-02-05 18:27:07 +0530

    Lazy column and data class

--- app/src/main/java/com/learning/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/learning/myshoppinglistapp/MainActivity.kt
@@ -6,15 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learning.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,11 +34,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
                     Column (modifier = Modifier.fillMaxSize(),
                         verticalArrangement = Arrangement.Center,
                         ) {
                         Button(onClick = { /*TODO*/ }, modifier = Modifier.align(CenterHorizontally)) {
                             Text(text = "Add Item")
+                        }
+                        LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+                            item(sItems) {
+
+                            }
                         }
                     }
                 }
@@ -39,3 +52,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+data class ShoppingItem(val id:Int, val name: String,
+                        var quantity: Int, var editable: Boolean = false)
