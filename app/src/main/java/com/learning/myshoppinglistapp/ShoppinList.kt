9fe8a63f1c69b3e701bb367184621092d4875554
Nagma Bano <nagma.bano@sap.com>
2024-02-08 19:25:13 +0530

    shopping list item

--- app/src/main/java/com/learning/myshoppinglistapp/ShoppinList.kt
+++ app/src/main/java/com/learning/myshoppinglistapp/ShoppinList.kt
@@ -1,5 +1,7 @@
 package com.learning.myshoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(val id:Int, val name: String,
@@ -41,7 +46,8 @@ fun shoppingListApp() {
         LazyColumn(modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)) {
-            item(sItems) {
+            items(sItems){
+                ShoppingListItem(it, {}, {})
             }
         }
     }
@@ -91,5 +97,21 @@ fun shoppingListApp() {
             }
 
         )
+    }
+}
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+        .border(
+            border = BorderStroke(2.dp, Color(0XFF018786)),
+            shape = RoundedCornerShape(20)
+        )) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
