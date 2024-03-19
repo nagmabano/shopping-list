5085a270beb4b3c9ccee5ea51e1373862838037b
Nagma Bano <bano.nagma.1@gmail.com>
2024-03-20 00:27:48 +0530

    added dependency for map, retrofit and navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,13 @@ android {
 
 dependencies {
 
+    implementation("com.google.maps.android:maps-compose:2.15.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
